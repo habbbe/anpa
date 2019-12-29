72bd3f3b97491c42adee93b57ae3b2960eba35c9
habbbe <habbbe@gmail.com>
2019-12-29 14:44:31 +0100

    Simplified json_parser to use less eat

--- test/json/json_parser.h
+++ test/json/json_parser.h
@@ -18,29 +18,29 @@ constexpr auto string_parser = []() {
     constexpr auto unicode = item<'u'>() >> times<4>(item_if([](const auto& f) {return std::isxdigit(f);}));
     constexpr auto escaped = item<'\\'>() >> (unicode || any_of<'"','\\','/','b','f','n','r','t'>());
     constexpr auto notEnd = escaped || not_item<'"'>();
-    return lift_value<json_string>(eat(item<'"'>()) >> many(notEnd) << item<'"'>());
+    return lift_value<json_string>(item<'"'>() >> many(notEnd) << item<'"'>());
 }();
 
-constexpr auto number_parser = eat(floating<json_number, true>());
-constexpr auto bool_parser = eat((seq<'t','r','u','e'>() >= true) ||
-                                 (seq<'f','a','l','s','e'>() >= false));
-constexpr auto null_parser = eat(seq<'n','u','l','l'>() >= json_null());
+constexpr auto number_parser = floating<json_number, true>();
+constexpr auto bool_parser = seq<'t','r','u','e'>() >> mreturn<true>() ||
+                                 seq<'f','a','l','s','e'>() >> mreturn<false>();
+constexpr auto null_parser = seq<'n','u','l','l'>() >= json_null();
 
 template <typename F>
 constexpr auto get_object_parser(F value_parser) {
     auto pair_parser = lift_value<json_object_pair>(string_parser, eat(item<':'>() >> value_parser));
-    return eat(item<'{'>()) >> many_to_map(pair_parser, eat(item<','>())) << eat(item<'}'>());
+    return item<'{'>() >> many_to_map(eat(pair_parser), eat(item<','>())) << eat(item<'}'>());
 }
 
 template <typename F>
 constexpr auto get_array_parser(F value_parser) {
-    return eat(item<'['>()) >> many_to_vector(value_parser, eat(item<','>())) << eat(item<']'>());
+    return item<'['>() >> many_to_vector(value_parser, eat(item<','>())) << eat(item<']'>());
 }
 
 constexpr auto json_parser = recursive<json_value>([](auto val_parser) {
-        return lift_or_value<json_value>(string_parser, number_parser,
+        return eat(lift_or_value<json_value>(string_parser, number_parser,
                                                 get_object_parser(val_parser), get_array_parser(val_parser),
-                                                bool_parser, null_parser);
+                                                bool_parser, null_parser));
 });
 
 constexpr auto array_parser = get_array_parser(json_parser);
